2648e8df1f53d1a5bc68ad480c1e125df822b882
Gabriel Anguiano <anguianog97@gmail.com>
2020-07-30 16:59:28 -0700

    successfully able to add a postgress db, but might have tweek some things

---
+++ database/migrations/20200730150109_MMAStore.js
@@ -0,0 +1,45 @@
+
+exports.up = function(knex, Promise) {
+  return (
+    knex.schema
+        .createTable('products', tbl => {
+            tbl.increments('product_id');
+
+            tbl.string('name', 255)
+            .notNullable()
+            .unique()
+
+            tbl.decimal('price')
+            .notNullable()
+
+            tbl.string('type', 255)
+            .notNullable()
+
+            tbl.string('gender', 6)
+            
+            tbl.string('description', 500)
+        })
+        /* end of product table */
+
+        // product images 
+
+       .createTable('product_images', tbl => {
+           tbl.increments('product_image_id')
+           tbl  
+            .integer('product_id')
+            .references('product_id')
+            .inTable('products')
+            .unsigned()
+            .notNullable()
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+       })
+    
+  )
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTableIfExists('product_images')
+    .dropTableIfExists('products')
+};
