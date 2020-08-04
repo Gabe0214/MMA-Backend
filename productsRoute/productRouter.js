63dcf33ffd4d9c10c6f7cffeaec95763215d5d26
Gabriel Anguiano <anguianog97@gmail.com>
2020-08-04 15:04:46 -0700

    get product by id route done

--- productsRoute/productRouter.js
+++ productsRoute/productRouter.js
@@ -5,37 +5,40 @@ const products = require('./productsModel')
 const router = express.Router()
 
 
-router.get('/', (req, res) => {
-    products.getAllProducts()
-    .then(items => { return items})
-    .then(items=> {
-        let allItems = [];
-        items.map(item => {
-           allItems.push({product_id: item.product_id, product_name: item.name, price: item.price, type: item.type , gender: item.gender, brand: item.brand, desc: item.description, images: [{image_one: item.img_source_1}, {image_two: item.image_source_2}, {image_three: item.image_source_3}]})
-        })
-        res.status(200).json(allItems)
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({message: 'Something went wrong :('})
-    })
+router.get('/', async (req, res) => {
+ 
+    try {
+        const allProducts = await products.getAllProducts()
+        let modifiedProducts = []
+        allProducts.map(product => {return modifiedProducts.push({product_id: product.product_id, product_name: product.name, price: product.price, type: product.type , gender: product.gender, brand: product.brand, desc: product.description, images: [{image_one: product.img_source_1}, {image_two: product.image_source_2}, {image_three: product.image_source_3}]})})
+        res.status(200).json(modifiedProducts)
+    }
+    catch(err) {
+        console.log(err)  
+    }
 })
 
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const productId = req.params.id;
+    
+    try {
+        let product = await products.getProductByID(productId)
+        if(product){res.status(200).json(product)}
+        else{ res.status(404).json({message: `Product with the given ID: ${productId} does not exist :(.`})}
+        
+    }
 
-    products.getProductImages(productId)
-    .then(item =>{
-        // console.log(items)
-        res.status(200).json(item)
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({message: 'Something went wrong :('})
-    })
+    catch (error){
+        res.status(500).json({message: "something went wrong in the server"})
+    }
+     
+   
 })
+
+
+
 
 
 module.exports = router;
--- productsRoute/productsModel.js
+++ productsRoute/productsModel.js
@@ -2,6 +2,7 @@ const db = require('../database/dbConfing')
 
 module.exports = {
     getAllProducts,
+    getProductByID
 }
 
 
@@ -11,5 +12,14 @@ function getAllProducts(){
     .select('images.product_id', 'p.name', 'p.brand', 'p.type', 'p.gender', 'p.price', 'p.description', 'images.img_source_1', 'images.image_source_2', 'images.image_source_3')
     .orderBy('images.product_id')
     
+}
+
+function getProductByID(id){
+  return db('products as p')
+    .join('product_images as images', 'images.product_id', 'p.product_id')
+    .select('images.product_id', 'p.name', 'p.brand', 'p.type', 'p.gender', 'p.price', 'p.description', 'images.img_source_1', 'images.image_source_2', 'images.image_source_3')
+    .orderBy('images.product_id')
+    .where('images.product_id', id)
+    .first()
 }
 
