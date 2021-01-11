3c23d22007b597b884b94bf7cd04ae39b4b4d46c
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-11 11:39:41 -0800

    refactored get all products endpoint, working on qry parmams

--- productsRoute/productRouter.js
+++ productsRoute/productRouter.js
@@ -1,148 +1,144 @@
-const express = require('express')
-const products = require('./productsModel')
+const express = require('express');
+const products = require('./productsModel');
 
-
-
-const router = express.Router()
-
+const router = express.Router();
 
 router.get('/', async (req, res) => {
- 
-    try {
-        const allProducts = await products.getAllProducts()
-        let modifiedProducts = []
-        allProducts.map(product => {return modifiedProducts.push({product_id: product.product_id, product_name: product.name, price: product.price, type: product.type , gender: product.gender, brand: product.brand, desc: product.description, images: [{image_one: product.img_source_1}, {image_two: product.image_source_2}, {image_three: product.image_source_3}]})})
-        const qry = req.query
+	try {
+		const allProducts = await products.getAllProducts();
+		const { product_for, type } = req.query;
+		console.log({ product_for, type });
+		// if (Object.keys(qry).length > 0) {
+		// 	console.log('hello');
+		// 	const filteredProducts = await products.filterProductsByQueryParams(qry);
+		// 	const qryModified = [];
+		// 	filteredProducts.map((product) => {
+		// 		return qryModified.push({
+		// 			product_id: product.product_id,
+		// 			product_name: product.name,
+		// 			price: product.price,
+		// 			type: product.type,
+		// 			gender: product.gender,
+		// 			brand: product.brand,
+		// 			desc: product.description,
+		// 			images: [
+		// 				{ image_one: product.img_source_1 },
+		// 				{ image_two: product.image_source_2 },
+		// 				{ image_three: product.image_source_3 }
+		// 			]
+		// 		});
+		// 	});
 
-        
-   
-
-
-        if(Object.keys(qry).length > 0) {
-            console.log('hello')
-            const filteredProducts =  await products.filterProductsByQueryParams(qry)
-            const qryModified = []
-            filteredProducts.map(product => {return qryModified.push({product_id: product.product_id, product_name: product.name, price: product.price, type: product.type , gender: product.gender, brand: product.brand, desc: product.description, images: [{image_one: product.img_source_1}, {image_two: product.image_source_2}, {image_three: product.image_source_3}]})})
-           
-          
-          res.status(200).json(qryModified)
-        }
-          else {res.status(200).json(modifiedProducts)}
-    }
-    catch(err) {
-        console.log(err)  
-    }
-})
-
-
+		// 	res.status(200).json(qryModified);
+		// } else {
+		res.status(200).json(allProducts);
+		// }
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 router.get('/:id', async (req, res) => {
-    const productId = req.params.id;
-    
-    try {
-        let product = await products.getProductAndImagesByID(productId)
-        if(product){
-            const { product_id, name, brand, type, gender, price, description, img_source_1, image_source_2, image_source_3 } = product;
-            res.status(200).json({product_id, name, brand, type, gender, price, description, images:[{img:img_source_1}, {img: image_source_2}, {img: image_source_3}]}) 
-        }
-        else{ res.status(404).json({message: `Product with the given ID: ${productId} does not exist :(.`})}
-        
-    }
-    catch (error){
-        res.status(500).json({message: "something went wrong in the server"})
-    }
-     
-   
-})
+	const productId = req.params.id;
 
+	try {
+		let product = await products.getProductAndImagesByID(productId);
+		if (product) {
+			const {
+				product_id,
+				name,
+				brand,
+				type,
+				gender,
+				price,
+				description,
+				img_source_1,
+				image_source_2,
+				image_source_3
+			} = product;
+			res.status(200).json({
+				product_id,
+				name,
+				brand,
+				type,
+				gender,
+				price,
+				description,
+				images: [ { img: img_source_1 }, { img: image_source_2 }, { img: image_source_3 } ]
+			});
+		} else {
+			res.status(404).json({ message: `Product with the given ID: ${productId} does not exist :(.` });
+		}
+	} catch (error) {
+		res.status(500).json({ message: 'something went wrong in the server' });
+	}
+});
 
-// DELETE product 
+// DELETE product
 
 router.delete('/:id', async (req, res) => {
-
-    try {
-         const deleteProduct = await products.deleteProduct(req.params.id)
-         res.status(200).json({message: "Product deleted"})
-    }
-
-    catch (err) {
-        console.log(err)
-        res.status(500).json({message: "something went wrong with the server"})
-    }
-})
-
+	try {
+		const deleteProduct = await products.deleteProduct(req.params.id);
+		res.status(200).json({ message: 'Product deleted' });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: 'something went wrong with the server' });
+	}
+});
 
 // Update a product
 
 router.put('/:id', async (req, res) => {
-   try{
-       const id = req.params.id;
-       const upDatedProduct = await products.upDateProduct(id, req.body)
-       res.status(200).json(upDatedProduct)
-      
-
-   }
-
-   catch (err){
-       console.log(err)
-       res.status(500).json({message: "something went wrong with the server"})
-   }
-})
-
+	try {
+		const id = req.params.id;
+		const upDatedProduct = await products.upDateProduct(id, req.body);
+		res.status(200).json(upDatedProduct);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: 'something went wrong with the server' });
+	}
+});
 
 //Update product image
 router.put('/product-image/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		const changes = req.body;
+		const upDatedProductImage = await products.upDateProductImages(id, changes);
+		if (id) {
+			res.status(200).json(upDatedProductImage);
+		} else {
+			res.status(404).json({ message: 'id does not exist :(' });
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: 'something went wrong in the server' });
+	}
+});
 
-
-  try {
-       const id = req.params.id
-       const changes = req.body
-       const upDatedProductImage = await products.upDateProductImages(id, changes)
-       if(id){ res.status(200).json(upDatedProductImage)}
-       else {
-           res.status(404).json({message: "id does not exist :("})
-       }
-  }
-
-  catch (err){
-      console.log(err)
-      res.status(500).json({message: "something went wrong in the server"})
-  }
-})
-
-
-// insert a product 
+// insert a product
 
 router.post('/', async (req, res) => {
-
-    try {
-          const data = req.body
-          const product = await products.insertProduct(data)
-          res.status(200).json(product)
-
-    }
-
-    catch (err) {
-        console.log(err)
-        res.status(500).json({message: "error something went wrong in the server"})
-    }
-})
+	try {
+		const data = req.body;
+		const product = await products.insertProduct(data);
+		res.status(200).json(product);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: 'error something went wrong in the server' });
+	}
+});
 
 // post product images
 
 router.post('/product-image', async (req, res) => {
-
-    try {
-        const insertedData = await products.insertProductImage(req.body)
-        res.status(200).json(insertedData)
-    }
-
-    catch (err) {
-      console.log(err)
-      res.status(500).json({message: "something went wrong in server"})
-    }
-})
-
-
+	try {
+		const insertedData = await products.insertProductImage(req.body);
+		res.status(200).json(insertedData);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: 'something went wrong in server' });
+	}
+});
 
 module.exports = router;
--- productsRoute/productsModel.js
+++ productsRoute/productsModel.js
@@ -1,91 +1,109 @@
-const db = require('../database/dbConfing')
+const db = require('../database/dbConfing');
 
 module.exports = {
-    getAllProducts,
-    getProductByID,
-    getProductAndImagesByID,
-    deleteProduct,
-    upDateProduct,
-    upDateProductImages,
-    insertProduct,
-    insertProductImage,
-    filterProductsByQueryParams
+	getAllProducts,
+	getProductByID,
+	getProductAndImagesByID,
+	deleteProduct,
+	upDateProduct,
+	upDateProductImages,
+	insertProduct,
+	insertProductImage,
+	filterProductsByQueryParams
+};
+
+function getAllProducts() {
+	return db('products as p').select('*').orderBy('p.product_id');
 }
 
-
-function getAllProducts(){
-    return db('products as p')
-    .join('product_images as images', 'images.product_id', 'p.product_id')
-    .select('images.product_id', 'p.name', 'p.brand', 'p.type', 'p.gender', 'p.price', 'p.description', 'images.img_source_1', 'images.image_source_2', 'images.image_source_3')
-    .orderBy('images.product_id')
-    
+function getProductAndImagesByID(id) {
+	console.log(id);
+	return db('products as p')
+		.join('product_images as images', 'images.product_id', 'p.product_id')
+		.select(
+			'images.product_id',
+			'p.name',
+			'p.brand',
+			'p.type',
+			'p.gender',
+			'p.price',
+			'p.description',
+			'images.img_source_1',
+			'images.image_source_2',
+			'images.image_source_3'
+		)
+		.orderBy('images.product_id')
+		.where('images.product_id', id)
+		.first();
 }
 
-function getProductAndImagesByID(id){
-    console.log(id)
-  return db('products as p')
-    .join('product_images as images', 'images.product_id', 'p.product_id')
-    .select('images.product_id', 'p.name', 'p.brand', 'p.type', 'p.gender', 'p.price', 'p.description', 'images.img_source_1', 'images.image_source_2', 'images.image_source_3')
-    .orderBy('images.product_id')
-    .where('images.product_id', id)
-    .first()
-    
+function deleteProduct(id) {
+	return db('products as p').delete().where('p.product_id', id);
 }
 
-function deleteProduct(id){
-    return db('products as p')
-    .delete().where('p.product_id', id)
+function upDateProduct(id, changes) {
+	return db('products').update(changes).where('products.product_id', id).then((res) => getProductAndImagesByID(id));
 }
 
-function upDateProduct(id, changes){
-    return db('products')
-   .update(changes)
-   .where('products.product_id', id)
-   .then(res => getProductAndImagesByID(id)) 
+function upDateProductImages(id, changes) {
+	return db('product_images')
+		.update(changes)
+		.where('product_images.product_id', id)
+		.then((res) => getProductAndImagesByID(id));
 }
 
-function upDateProductImages(id, changes){
-    return db('product_images')
-    .update(changes)
-    .where('product_images.product_id', id)
-    .then(res => getProductAndImagesByID(id))
+function getProductByID(id) {
+	return db('products').where('products.product_id', id).orderBy('products.product_id').first();
 }
 
-
-function getProductByID(id){
-    return db('products')
-    .where('products.product_id', id)
-    .orderBy('products.product_id')
-    .first()
-
+function insertProduct(data) {
+	return db('products').insert(data, 'products.product_id').then(([ id ]) => {
+		return getProductByID(id);
+	});
 }
 
-function insertProduct(data){
-    return db('products')
-    .insert(data, "products.product_id")
-    .then(([id]) => {return getProductByID(id)})
+function insertProductImage(data) {
+	return db('product_images').insert(data, 'product_images.product_id').then(([ id ]) => {
+		return getProductAndImagesByID(id);
+	});
 }
 
-function insertProductImage(data){
-    return db('product_images')
-    .insert(data, "product_images.product_id")
-    .then(([id]) => { return getProductAndImagesByID(id)})
-}
-
-function filterProductsByQueryParams(qry){
-    const keys = Object.keys(qry)
-    if(keys.length == 1){
-        return db('products as p')
-        .join('product_images as images', 'images.product_id', 'p.product_id')
-        .select('images.product_id', 'p.name', 'p.brand', 'p.type', 'p.gender', 'p.price', 'p.description', 'images.img_source_1', 'images.image_source_2', 'images.image_source_3')
-        .where(qry)
-        .orderBy('p.product_id')
-    } else {
-        return db('products as p')
-        .join('product_images as images', 'images.product_id', 'p.product_id')
-        .select('images.product_id', 'p.name', 'p.brand', 'p.type', 'p.gender', 'p.price', 'p.description', 'images.img_source_1', 'images.image_source_2', 'images.image_source_3')
-        .where(keys[0], qry[keys[0]])
-        .andWhere(keys[1], qry[keys[1]])
-        .orderBy('p.product_id')
-    }
+function filterProductsByQueryParams(qry) {
+	const keys = Object.keys(qry);
+	if (keys.length == 1) {
+		return db('products as p')
+			.join('product_images as images', 'images.product_id', 'p.product_id')
+			.select(
+				'images.product_id',
+				'p.name',
+				'p.brand',
+				'p.type',
+				'p.gender',
+				'p.price',
+				'p.description',
+				'images.img_source_1',
+				'images.image_source_2',
+				'images.image_source_3'
+			)
+			.where(qry)
+			.orderBy('p.product_id');
+	} else {
+		return db('products as p')
+			.join('product_images as images', 'images.product_id', 'p.product_id')
+			.select(
+				'images.product_id',
+				'p.name',
+				'p.brand',
+				'p.type',
+				'p.gender',
+				'p.price',
+				'p.description',
+				'images.img_source_1',
+				'images.image_source_2',
+				'images.image_source_3'
+			)
+			.where(keys[0], qry[keys[0]])
+			.andWhere(keys[1], qry[keys[1]])
+			.orderBy('p.product_id');
+	}
 }
