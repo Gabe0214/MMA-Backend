c7fc3723bf6c697ad5b3c88eccacff91aa3b1863
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-12 12:58:32 -0800

    validation middleware for product fields complete.

---
+++ middleware/productsMiddleware.js
@@ -0,0 +1,42 @@
+function validateFields(req, res, next) {
+	const { name, price, type, product_for, description, brand, image, color } = req.body;
+
+	if (name == '' || !name) {
+		res.status(401).json({ message: 'name field is required' });
+	}
+
+	if (price == '' || !price) {
+		res.status(401).json({ message: 'price field is required' });
+	}
+
+	if (type == '' || !type) {
+		res.status(401).json({ message: 'type field is required' });
+	}
+
+	if (product_for == '' || !product_for) {
+		res.status(401).json({ message: 'product_for field is required' });
+	}
+
+	if (description == '' || !description) {
+		res.status(401).json({ message: 'description field is required' });
+	}
+
+	if (brand == '' || !brand) {
+		res.status(401).json({ message: 'brand field is required' });
+	}
+
+	if (image == '' || !image) {
+		res.status(401).json({ message: 'image field is required' });
+	}
+
+	if (color == '' || !color) {
+		res.status(401).json({ message: 'color field is required' });
+	}
+
+	next();
+	return;
+}
+
+module.exports = {
+	validateFields
+};
