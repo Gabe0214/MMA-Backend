036d6d3954fcd340b358faca908e8d72f3b39fef
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-21 18:56:07 -0800

    changed env in knex file

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,43 @@
 // Update with your config settings.
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
+	development: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	},
 
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URI,
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    }
-  },
+	testing: {
+		client: 'sqlite3',
+		connection: ':memory',
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run('PRAGMA foreign_keys = ON', done);
+			}
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/migrations'
+		}
+	},
 
-  testing: {
-    client: 'sqlite3',
-    connection: ':memory',
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      }
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory : './database/migrations'
-    },
-    seeds: {
-      directory: './database/migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URI,
-    migrations: {
-      directory: './database/migrations'
-    },
-    seeds: {
-      directory: './database/seeds'
-    }
-  }
-
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	}
 };
