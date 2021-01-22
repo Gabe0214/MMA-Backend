118fdd341586bcea2bc5144b641c40e8a6fb62fa
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-22 11:01:20 -0800

    still working on it

--- knexfile.js
+++ knexfile.js
@@ -37,7 +37,7 @@ module.exports = {
 			min: 2,
 			max: 10
 		},
-		ssl: 'require',
+		sslmode: process.env.NODE_ENV === 'production' ? 'require' : 'disable',
 		migrations: {
 			directory: './database/migrations'
 		},
