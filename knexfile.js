6bed4cad7a307e803a48ec0a33c1e8b54b83678d
Gabriel Anguiano <anguianog97@gmail.com>
2021-01-22 09:35:39 -0800

    configured ssl to true in production, currently having issues

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,10 @@ module.exports = {
 
 	production: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL,
+		connection: {
+			host: process.env.DATABASE_URL,
+			ssl: true
+		},
 		migrations: {
 			directory: './database/migrations'
 		},
